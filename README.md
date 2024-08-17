2f387495c3da530e77b27767838e0d82a462575a
ThomasEllis1994 <ThomasGEllis004@gmail.com>
2024-08-17 15:43:33 -0400

    updating to use with Exercise-5

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Module-10-Testing
+
 Tests for Module 10 of IOD
+
+link for exercise 4: http://3.19.76.28/
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = 8080;
 
 app.use(express.json());
 
 app.use("/", (req, res) => {
-    res.send({ status: 200, message: "Module-10 Expirimentation test-3" });
+    res.send({ status: 200, message: "Module-10 Exercise 2 complete" });
 });
 
 app.listen(port, () => {
